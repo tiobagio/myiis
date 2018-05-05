2d99a493d04bc57bdbd053d76c4ecd588e5d9179
anthonygrees <anthony_rees01@hotmail.com>
2018-05-05 17:09:30 +1000

    Added Ohai

--- recipes/server.rb
+++ recipes/server.rb
@@ -3,7 +3,11 @@ powershell_script 'Install IIS' do
 end
 
 file 'C:\inetpub\wwwroot\Default.htm' do
-  content '<h1>Hello, world Windows!</h1>'
+  content "<h1>Hello, world Windows!</h1>
+  <h2>ipaddress: #{node['ipaddress']}</h2>
+  <h2>hostname: #{node['hostname']}</h2>
+  <h2>total memory: #{node['memory']['total']}</h2>
+  <h2>CPU Mhz: #{node['cpu']['0']['mhz']}</h2>"
 end
 
 service 'w3svc' do
